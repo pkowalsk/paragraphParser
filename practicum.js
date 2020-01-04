4d443499b922746e48732dc3f0a76c9a3e448ee5
Pete K <pkowalsk@users.noreply.github.com>
2020-01-04 01:42:14 -0500

    formatting fixes

--- practicum.js
+++ practicum.js
@@ -1,6 +1,4 @@
-let paragraph = 'Take this paragraph of text and return an alphabetized list of ALL unique words. A unique word is any form of a word often communicated with essentially the same meaning. For example, dish and dishes could be defined as a unique word by using their stem dish. For each unique word found in this entire paragraph, determine the how many times the word appears in total. Also, provide an analysis of what sentence index position or positions the word is found. The following words should not be included in your analysis or result set: "a", "the", "and", "of", "in", "be", "also" and "as"! Your final result MUST be displayed in a readable console output in the same format as the JSON sample object shown below.';
-
-let formatParagraph = paragraph => {
+function formatParagraph (paragraph) {
 	// get array of words seperated by space for word array
 	let wordPara = paragraph.replace(/[.]/g,'');
 
@@ -20,15 +18,15 @@ let formatParagraph = paragraph => {
 	return wordPara;
 };
 
-let handleStemWords = wordSet => {
+function handleStemWords (wordSet) {
 	// treat pluralized words (ie: 'dish','dishes') as the same word. do this by replacing words with endings of 's' or 'es' as the same as those without those endings
 	// loop over word array and compare each element to the array, with an index occurring after the selected index (to avoid repetition)
 	//wordPara = wordPara.replace(/([a-zA-Z]+?)(s )/g, '$1 ');
-	for(var i = 0; i < wordSet.length; i++){
-		var stem = wordSet[i]; // set the word stem equal to the element
+	for(let i = 0; i < wordSet.length; i++){
+		let stem = wordSet[i]; // set the word stem equal to the element
 
 		for(var x = i+1; x < wordSet.length; x++){
-			if( stem == wordSet[x].slice(0,stem.length) // check if the current element has a stem of the selected word
+			if( stem === wordSet[x].slice(0,stem.length) // check if the current element has a stem of the selected word
 				&& (
 					( wordSet[x].slice(wordSet[x].length-1) == 's' && wordSet[x].length == stem.length + 1) // check if last character is s and element is only 1 char longer 
 					|| (wordSet[x].slice(wordSet[x].length-2) == 'es' && wordSet[x].length == stem.length + 2) // check if last character is es and element is only 2 char longer
@@ -44,15 +42,15 @@ let handleStemWords = wordSet => {
 	return wordSet;
 };
 
-let getWordCount = wordSet => {
+function getWordCount (wordSet) {
 	let curWord = '',
 		count = 0,
 		curWordObj = {},
 		wordArray = [];
 
 	// loop over words and get count of duplicates
-	for(var i=0; i < wordSet.length; i++){
-		if (curWord != wordSet[i]){
+	for(let i=0; i < wordSet.length; i++){
+		if (curWord !== wordSet[i]){
 			curWord = wordSet[i];
 			count = 1;
 			curWordObj = {'word': curWord, 'total-occurences': 1};
@@ -66,7 +64,7 @@ let getWordCount = wordSet => {
 	return wordArray;
 };
 
-let getSentencePosition = (wordArray, sentenceArray) => {
+function getSentencePosition (wordArray, sentenceArray) {
 	// loop through words and get positions in the sentences
 	for(var i=0; i < wordArray.length; i++){
 		let sentPos = []; // array for found sentence indexes
@@ -82,6 +80,7 @@ let getSentencePosition = (wordArray, sentenceArray) => {
 	return wordArray;
 };
 
+let paragraph = 'Take this paragraph of text and return an alphabetized list of ALL unique words. A unique word is any form of a word often communicated with essentially the same meaning. For example, dish and dishes could be defined as a unique word by using their stem dish. For each unique word found in this entire paragraph, determine the how many times the word appears in total. Also, provide an analysis of what sentence index position or positions the word is found. The following words should not be included in your analysis or result set: "a", "the", "and", "of", "in", "be", "also" and "as"! Your final result MUST be displayed in a readable console output in the same format as the JSON sample object shown below.';
 // convert to lowercase to avoid words like 'cat' and 'Cat' from showing up twice
 paragraph = paragraph.toLowerCase();
 
@@ -109,15 +108,6 @@ let wordArray = getWordCount(wordSet);
 // get position of words in sentences
 wordArray = getSentencePosition(wordArray, aSentences);
 
-let result = { results: wordArray };
-
-//creating a json object
-let jObject={};
-for(i in wordArray)
-{
-    jObject[i] = wordArray[i];
-}
+const result = { results: wordArray };
 
 console.log(result);
-let prettyJson = JSON.stringify(result, 0, 2);
-$('#results').html(prettyJson);
